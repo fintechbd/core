0f438aa6cbab60fd260b5d323855c586cab1446e
hafijul233 <hafijul233@users.noreply.github.com>
2023-10-07 11:16:44 +0000

    Fix styling

--- src/Observers/BlameableObserver.php
+++ src/Observers/BlameableObserver.php
@@ -2,7 +2,6 @@
 
 namespace Fintech\Core\Observers;
 
-
 class BlameableObserver
 {
     /**
--- src/Supports/Constant.php
+++ src/Supports/Constant.php
@@ -4,7 +4,7 @@ namespace Fintech\Core\Supports;
 
 class Constant
 {
-    const SORT_DIRECTIONS = ['asc', 'desc'];
+    public const SORT_DIRECTIONS = ['asc', 'desc'];
 
-    const PAGINATE_LENGTHS = [10 => '10', 20 => '20', 50 => '50', 100 => '100', 500 => '500'];
+    public const PAGINATE_LENGTHS = [10 => '10', 20 => '20', 50 => '50', 100 => '100', 500 => '500'];
 }
