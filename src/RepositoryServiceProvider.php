0b04fbef953125188e20df651a332bf782907ea0
Mohammad Hafijul Islam <hafijul233@gmail.com>
2023-10-10 10:50:46 +0600

    stub files updated

---
+++ src/RepositoryServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Fintech\Core;
+
+use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider implements DeferrableProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        foreach (Config::get('fintech.auth.repositories') as $interface => $binding) {
+            $this->app->bind($interface, function ($app) use ($binding) {
+                return $app->make($binding);
+            });
+        }
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array<int, string>
+     */
+    public function provides(): array
+    {
+        return array_keys(Config::get('fintech.auth.repositories'));
+    }
+}
