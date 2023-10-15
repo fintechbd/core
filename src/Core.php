1dd9e5fd93b8f629eba90a8b02e43b216e6ea83e
Mohammad Hafijul Islam <hafijul233@gmail.com>
2023-10-15 16:29:06 +0600

    LP-1 MetaData module dependency removed

--- src/Core.php
+++ src/Core.php
@@ -13,4 +13,15 @@ class Core
     {
         return app(SettingService::class);
     }
+
+    /**
+     * verify if a available addon or package is installed
+     *
+     * @param string $name
+     * @return bool
+     */
+    public function packageExists(string $name): bool
+    {
+        return class_exists("\Fintech\{$name}\Facades\{$name}::class");
+    }
 }
