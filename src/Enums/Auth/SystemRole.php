9556025c55855e5732ddb6c5154f61b55bfdf351
Mohammad Hafijul Islam <hafijul233@gmail.com>
2023-11-22 05:09:32 +0600

    master and system user created when service country added in metadata

---
+++ src/Enums/Auth/SystemRole.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Fintech\Core\Enums\Auth;
+
+use Fintech\Core\Traits\HasSerialization;
+
+enum SystemRole:string
+{
+    use HasSerialization;
+
+    case SuperAdmin = 'Super Admin';
+    case MasterUser = 'Master User';
+
+}
