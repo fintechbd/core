1accdba733369fb1692bf2882109cd68a1cef565
Mohammad Hafijul Islam <hafijul233@gmail.com>
2023-11-22 02:13:04 +0600

    catalog data handle api added

---
+++ src/Enums/MetaData/CatalogType.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Fintech\Core\Enums\MetaData;
+
+use Fintech\Core\Traits\HasSerialization;
+
+enum CatalogType: string
+{
+    use HasSerialization;
+    case Gender = 'gender';
+    case BloodGroup = 'blood-group';
+    case MaritalStatus = 'marital-status';
+}
