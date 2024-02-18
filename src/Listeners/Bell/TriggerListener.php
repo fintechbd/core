3f60640b203c7a32d238d095c8b4b1dfd254284b
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-02-19 02:44:57 +0600

    bell event listener added

---
+++ src/Listeners/Bell/TriggerListener.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Fintech\Core\Listeners\Bell;
+
+class TriggerListener
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(object $event): void
+    {
+        //
+    }
+}
