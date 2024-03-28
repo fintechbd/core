50e9c53c860bae9fb0bb95f1073185f1eb57e055
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-03-29 03:19:34 +0600

    typo fixed

--- src/Enums/MetaData/CatalogType.php
+++ src/Enums/MetaData/CatalogType.php
@@ -8,7 +8,13 @@ enum CatalogType: string
 {
     use HasSerialization;
 
-    case Gender = 'gender';
+    case Gender = 'gender'; //done
     case BloodGroup = 'blood-group';
-    case MaritalStatus = 'marital-status';
+    case MaritalStatus = 'marital-status'; //done
+    case FundSource = 'fund-source'; //done
+    case IdentityDocument = 'id-document'; //done
+    case AddressProof = 'address-proof'; //done
+    case Occupation = 'occupation'; //done
+    case Relation = 'relation';//done
+    case RemittancePurpose = 'remittance-purpose'; //done
 }
