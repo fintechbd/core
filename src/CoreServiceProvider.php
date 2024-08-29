fee1aee2985c9ae2eca46ab58b4ae25cee80e8d2
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-08-30 00:05:55 +0600

    core service provider updated

--- src/CoreServiceProvider.php
+++ src/CoreServiceProvider.php
@@ -2,28 +2,13 @@
 
 namespace Fintech\Core;
 
-use Fintech\Core\Commands\InstallCommand;
-use Fintech\Core\Facades\Core;
-use Fintech\Core\Http\Middlewares\EncryptedRequestResponse;
-use Fintech\Core\Http\Middlewares\HttpLogger;
-use Fintech\Core\Http\Middlewares\ImposterCheck;
-use Fintech\Core\Providers\EventServiceProvider;
-use Fintech\Core\Providers\MacroServiceProvider;
-use Fintech\Core\Providers\RepositoryServiceProvider;
 use Fintech\Core\Supports\Utility;
-use Fintech\Core\Traits\RegisterPackageTrait;
-use Illuminate\Database\Events\QueryExecuted;
-use Illuminate\Routing\Router;
-use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Str;
 
 class CoreServiceProvider extends ServiceProvider
 {
-    use RegisterPackageTrait;
+    use \Fintech\Core\Traits\RegisterPackageTrait;
 
     /**
      * Register any application services.
@@ -39,37 +24,16 @@ class CoreServiceProvider extends ServiceProvider
             'fintech.core'
         );
 
-        Config::set('database.connections.mongodb', [
-            'driver' => 'mongodb',
-            'dsn' => env('DATABASE_URL', 'mongodb+srv://forge:forge@localhost/myappdb?retryWrites=true&w=majority'),
-            'database' => env('DB_DATABASE', 'mongodb'),
-        ]);
-
-        Config::set('logging.channels.query', [
-            'driver' => 'daily',
-            'path' => storage_path('logs/query.log'),
-            'level' => env('LOG_LEVEL', 'debug'),
-            'days' => 14,
-            'replace_placeholders' => true,
-        ]);
-
-        Config::set('filesystems.disks.source', [
-            'driver' => 'local',
-            'root' => base_path(),
-            'visibility' => 'public',
-            'throw' => false,
-        ]);
-
-        $this->app->register(EventServiceProvider::class);
-        $this->app->register(RepositoryServiceProvider::class);
-        $this->app->register(MacroServiceProvider::class);
+        $this->app->register(\Fintech\Core\Providers\EventServiceProvider::class);
+        $this->app->register(\Fintech\Core\Providers\RepositoryServiceProvider::class);
+        $this->app->register(\Fintech\Core\Providers\MacroServiceProvider::class);
     }
 
     /**
      * Bootstrap any package services.
-     * @param Router $router
+     * @param \Illuminate\Routing\Router $router
      */
-    public function boot(Router $router): void
+    public function boot(\Illuminate\Routing\Router $router): void
     {
         $this->loadSettings();
 
@@ -96,39 +60,38 @@ class CoreServiceProvider extends ServiceProvider
 
         if ($this->app->runningInConsole()) {
             $this->commands([
-                InstallCommand::class,
+                \Fintech\Core\Commands\InstallCommand::class,
             ]);
         }
 
-
         $this->loadQueryLogger();
 
-        $router->middlewareGroup('encrypted', [EncryptedRequestResponse::class])
-            ->middlewareGroup('http_log', [HttpLogger::class])
-            ->middlewareGroup('imposter', [ImposterCheck::class]);
+        $router->middlewareGroup('encrypted', [\Fintech\Core\Http\Middlewares\EncryptedRequestResponse::class])
+            ->middlewareGroup('http_log', [\Fintech\Core\Http\Middlewares\HttpLogger::class])
+            ->middlewareGroup('imposter', [\Fintech\Core\Http\Middlewares\ImposterCheck::class]);
     }
 
     private function loadSettings(): void
     {
-        if (!App::environment('testing')) {
-            Core::setting()->list()->each(function ($setting) {
-                Config::set("fintech.{$setting->package}.{$setting->key}", Utility::typeCast($setting->value, $setting->type));
-            });
+        if (\Illuminate\Support\Facades\Schema::getConnection()) {
+            if (\Illuminate\Support\Facades\Schema::hasTable('settings')) {
+                \Fintech\Core\Facades\Core::setting()->list()->each(function ($setting) {
+                    Config::set("fintech.{$setting->package}.{$setting->key}", Utility::typeCast($setting->value, $setting->type));
+                });
+            }
         }
     }
 
     private function loadQueryLogger(): void
     {
         if (Config::get('fintech.core.query_logger_enabled') && Config::get('database.default') != 'mongodb') {
-            DB::listen(function (QueryExecuted $event) {
-
+            \Illuminate\Support\Facades\DB::listen(function (\Illuminate\Database\Events\QueryExecuted $event) {
                 //skip console query logging
                 if ($this->app->runningInConsole() && !config('fintech.core.log_console_query')) {
                     return;
                 }
-
-                $query = Str::replaceArray('?', $event->bindings, $event->sql);
-                Log::channel('query')
+                $query = \Illuminate\Support\Str::replaceArray('?', $event->bindings, $event->sql);
+                \Illuminate\Support\Facades\Log::channel('query')
                     ->debug("TIME: {$event->time} ms, SQL: {$query}");
             });
         }
