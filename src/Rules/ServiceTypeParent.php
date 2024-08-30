005cbcd0728d9a76fc9fcc343688049f7e6f7467
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-08-31 03:30:13 +0600

    service type parent status check rule added

---
+++ src/Rules/ServiceTypeParent.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Fintech\Core\Rules;
+
+use Closure;
+use Fintech\Business\Facades\Business;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class ServiceTypeParent implements ValidationRule
+{
+    /**
+     * @param string $type
+     */
+    public function __construct(private string $type = 'yes')
+    {
+
+    }
+
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        if ($value != null) {
+            $serviceType = Business::serviceType()->find($value);
+            if ($serviceType->service_type_id_parent != $this->type) {
+                $fail("The parent service type {$serviceType->service_type_name} needs to be set ({$this->type}).");
+            }
+        }
+    }
+}
