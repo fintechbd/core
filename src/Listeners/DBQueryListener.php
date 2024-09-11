4c24dce6896dc18c113a9fadbed47a9ff8d262d5
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-09-12 01:12:44 +0600

    syn job dispatch added

--- src/Listeners/DBQueryListener.php
+++ src/Listeners/DBQueryListener.php
@@ -2,28 +2,20 @@
 
 namespace Fintech\Core\Listeners;
 
-use Illuminate\Database\Events\QueryExecuted;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Str;
 
 class DBQueryListener
 {
-    /**
-     * Handle the event.
-     */
-    public function handle(QueryExecuted $event): void
+    public function handle(\Illuminate\Database\Events\QueryExecuted $event): void
     {
-        if (Config::get('fintech.core.query_logger_enabled') && Config::get('database.default') != 'mongodb') {
-            DB::listen(function (QueryExecuted $event) {
-                if ($this->app->runningInConsole() && !config('fintech.core.log_console_query')) {
-                    return;
-                }
-                $query = Str::replaceArray('?', $event->bindings, $event->sql);
-                Log::channel('query')
-                    ->debug("TIME: {$event->time} ms, SQL: {$query}");
-            });
+        if (\config('fintech.core.query_logger_enabled') && \config('database.default') != 'mongodb') {
+
+            if (app()->runningInConsole() && !\config('fintech.core.log_console_query')) {
+                return;
+            }
+
+            $query = \Illuminate\Support\Str::replaceArray('?', $event->bindings, $event->sql);
+
+            \Illuminate\Support\Facades\Log::channel('query')->debug("TIME: {$event->time} ms, SQL: {$query}");
         }
     }
 }
