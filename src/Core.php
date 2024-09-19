0ace8e3c9abe123f56286e269651269685768b1e
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-09-20 02:37:55 +0600

    unused php doc block removed

--- src/Core.php
+++ src/Core.php
@@ -10,48 +10,32 @@ use Fintech\Core\Services\SettingService;
 
 class Core
 {
-    /**
-     * @return SettingService
-     */
     public function setting($filters = null)
     {
         return \singleton(SettingService::class, $filters);
     }
 
-    /**
-     * @return ApiLogService
-     */
     public function apiLog($filters = null)
     {
         return \singleton(ApiLogService::class, $filters);
     }
 
-    /**
-     * @return FailedJobService
-     */
     public function failedJob($filters = null)
     {
         return \singleton(FailedJobService::class, $filters);
     }
 
-    /**
-     * @return JobService
-     */
     public function job($filters = null)
     {
         return \singleton(JobService::class, $filters);
     }
 
-    /**
-     * @return ScheduleService
-     */
     public function schedule($filters = null)
     {
         return \singleton(ScheduleService::class, $filters);
     }
 
     //** Crud Service Method Point Do not Remove **//
-
 
     /**
      * verify if a available addon or package is installed
