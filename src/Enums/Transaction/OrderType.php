99bb606f1fc3362cbcb4d09870fab475dcf0695e
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-09-26 14:38:07 +0600

    order type added in all order

--- src/Enums/Transaction/OrderType.php
+++ src/Enums/Transaction/OrderType.php
@@ -2,14 +2,26 @@
 
 namespace Fintech\Core\Enums\Transaction;
 
+use Fintech\Core\Traits\EnumHasSerialization;
+
 enum OrderType: string implements \JsonSerializable
 {
+    use EnumHasSerialization;
+
     case BankDeposit = 'bank_deposit';
     case CardDeposit = 'card_deposit';
     case InteracDeposit = 'interac_e_transfer';
     case BankTransfer = 'bank_transfer';
     case CashPickup = 'cash_pickup';
     case WalletTransfer = 'wallet_transfer';
+    case Airtime = 'airtime';
+    case BillPayment = 'bill_payment';
+    case WalletToWallet = 'wallet_to_wallet';
+    case CurrencySwap = 'currency_swap';
+    case RequestMoney = 'request_money';
+    case WalletToPrepaidCard = 'wallet_to_prepaid_card';
+    const WalletToBank = 'wallet_to_bank';
+    const WalletToAtm = 'wallet_to_atm';
 
     /**
      * @return mixed
