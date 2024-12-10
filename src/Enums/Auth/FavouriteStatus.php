2f4523cbdc2843fff1327566fea43362b0ba2474
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-12-10 18:03:07 +0600

    favourite list issue update

--- src/Enums/Auth/FavouriteStatus.php
+++ src/Enums/Auth/FavouriteStatus.php
@@ -3,19 +3,22 @@
 namespace Fintech\Core\Enums\Auth;
 
 use Fintech\Core\Attributes\Enumeration;
+use Fintech\Core\Enums\Color;
 use Fintech\Core\Traits\EnumHasSerialization;
 
 enum FavouriteStatus: string implements \JsonSerializable
 {
     use EnumHasSerialization;
 
-    #[Enumeration(color: 'primary', description: 'Favourite request has been sent.')]
+    #[Enumeration(color: Color::Yellow500, description: 'Favourite request has been sent.')]
     case Requested = 'requested';
-    #[Enumeration(color: 'success', description: 'Favourite request has been accepted.')]
+
+    #[Enumeration(color: Color::Green500, description: 'Favourite request has been accepted.')]
     case Accepted = 'accepted';
-    #[Enumeration(color: 'danger', description: 'Favourite request has been declined.')]
+
+    #[Enumeration(color: Color::Red500, description: 'Favourite request has been declined.')]
     case Rejected = 'rejected';
-    #[Enumeration(color: 'secondary', description: 'Favourite request has blocked the user.')]
+    #[Enumeration(color: Color::Gray500, description: 'Favourite request has blocked the user.')]
     case Blocked = 'blocked';
 
 }
