5af88c63b1e54163565fb2aa679bde8cdbf65224
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-12-19 12:46:30 +0600

    confirm request money endpoint added

---
+++ src/Enums/Reload/RequestMoneyConfirm.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Fintech\Core\Enums\Reload;
+
+use Fintech\Core\Attributes\Enumeration;
+use Fintech\Core\Enums\Color;
+use Fintech\Core\Traits\EnumHasSerialization;
+
+/**
+ * N.B: status value must have to match with order status
+ */
+enum RequestMoneyConfirm: string implements \JsonSerializable
+{
+    use EnumHasSerialization;
+
+    #[Enumeration(color: Color::Orange500)]
+    case Pending = 'pending';
+
+    #[Enumeration(color: Color::Purple500)]
+    case Processing = 'processing';
+
+    #[Enumeration(color: Color::Green500)]
+    case Accepted = 'accepted';
+
+    #[Enumeration(color: Color::Red500)]
+    case Rejected = 'rejected';
+
+    #[Enumeration(color: Color::Slate500)]
+    case Cancelled = 'cancelled';
+
+    #[Enumeration(color: Color::Yellow500, label: 'Reviewing')]
+    case AdminVerification = 'admin_verification';
+
+
+}
