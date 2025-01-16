76e5a2b6df71539dea14db2ab7895a3a49d133da
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-01-16 13:42:56 +0600

    template crud completed

--- config/changelog.php
+++ config/changelog.php
@@ -1,15 +1,15 @@
 <?php
 
 use Fintech\Core\Commands\AppUpdateCommand;
+use Illuminate\Support\Facades\Artisan;
 
 return [
 //    '1.0.5' => function (AppUpdateCommand $command) {
 //
 //    },
-//    '1.0.4' => function (AppUpdateCommand $command) {
-//        dump(get_class($command));
-//        //        $this->call('db:seed', ['--class' => \Fintech\Transaction\Seeders\PolicySeeder::class]);
-//    },
+    '1.0.4' => function (AppUpdateCommand $command) {
+        Artisan::call('migrate');
+    },
     '1.0.3' => function (AppUpdateCommand $command) {
         if (\Fintech\Core\Facades\Core::packageExists('Business')) {
             \Fintech\Business\Facades\Business::serviceType()->list()->each(function ($service) use ($command) {
