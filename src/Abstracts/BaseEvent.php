d897a0abf04db3c696da77bf40cc3e594bebaf29
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-01-20 11:16:31 +0600

    event call error

--- src/Abstracts/BaseEvent.php
+++ src/Abstracts/BaseEvent.php
@@ -4,6 +4,45 @@ namespace Fintech\Core\Abstracts;
 
 abstract class BaseEvent
 {
-    public $aliases = [];
+    public $variables = [];
+
+    public $ipAddress;
+
+    public $agent;
+
+    protected function init(): void
+    {
+
+        $this->ip();
+        $this->userAgent();
+        $this->variables();
+    }
+
+    public function ip(): ?string
+    {
+        if ($this->ipAddress == null && !app()->runningInConsole()) {
+            $this->ipAddress = request()->ip();
+        }
+
+        return $this->ipAddress;
+    }
+
+    public function userAgent(): ?string
+    {
+        if ($this->agent == null && !app()->runningInConsole()) {
+            $this->agent = request()->userAgent();
+        }
+
+        return $this->agent;
+    }
+
+    public function variables(): array
+    {
+        if (empty($this->variables)) {
+            $this->variables = $this->aliases();
+        }
+
+        return $this->variables;
+    }
 
 }
--- src/Interfaces/Bell/HasDynamicString.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Fintech\Core\Interfaces\Bell;
-
-interface HasDynamicString
-{
-    public function aliases(): array;
-}
