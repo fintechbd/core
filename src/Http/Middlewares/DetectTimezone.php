8f85cfe579ce06325224cdd7fdfe47a357b68f54
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-03-02 13:57:18 +0600

    timezone added

---
+++ src/Http/Middlewares/DetectTimezone.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Fintech\Core\Http\Middlewares;
+
+use Closure;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DetectTimezone
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Closure(Request): (Response) $next
+     * @return JsonResponse|mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+//        if ($request->hasHeader('Timestamp')) {
+//            $datetime = $request->header('Timestamp', now()->format('c'));
+//            app()->setLocale($locale);
+//        }
+
+        return $next($request);
+    }
+
+}
