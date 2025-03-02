8c7347db166f96b4db93e2120cb990fa07b60d7b
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-03-02 13:23:41 +0600

    Locale added

--- src/Http/Middlewares/Localization.php
+++ src/Http/Middlewares/Localization.php
@@ -17,48 +17,10 @@ class Localization
      */
     public function handle(Request $request, Closure $next)
     {
-        //        if ($request->hasHeader('Locale')) {
-        //            $locale = $request->header('Locale');
-        //
-        //        }
-        //
-        //        if (config('fintech.core.encrypt_response', false)) {
-        //
-        //            $headerSignature = $request->header('Locale');
-        //
-        //            if (!$headerSignature) {
-        //                abort(Response::HTTP_BAD_REQUEST, 'Request header signature is missing.');
-        //            }
-        //
-        //            $cipher = $request->input('payload', '');
-        //
-        //            $payload = Encryption::decrypt($cipher);
-        //
-        //            $payloadSignature = hash('sha256', json_encode($payload));
-        //
-        //            if ($payloadSignature != $headerSignature) {
-        //                abort(Response::HTTP_NOT_ACCEPTABLE, 'Request payload signature is invalid.');
-        //            }
-        //
-        //            $request->offsetUnset('payload');
-        //
-        //            $request->merge($payload);
-        //        }
-        //
-        //        $response = $next($request);
-        //
-        //        if (config('fintech.core.encrypt_response', false) && $response instanceof JsonResponse) {
-        //
-        //            $signature = hash('sha256', $response->content());
-        //
-        //            $response->header('Signature', $signature);
-        //
-        //            $response->setContent(json_encode([
-        //                'payload' => Encryption::encrypt($response->content())
-        //            ]));
-        //        }
-        //
-        //        return $response;
+        if ($request->hasHeader('Language')) {
+            $locale = $request->header('Language', app()->getLocale());
+            app()->setLocale($locale);
+        }
 
         return $next($request);
     }
