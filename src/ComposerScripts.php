7ffada78f26407e1940f9ecb00e561c2fb5f162c
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-03-10 01:01:32 +0600

    composer reqs updated

---
+++ src/ComposerScripts.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Fintech\Core;
+
+class ComposerScripts
+{
+    /**
+     * Handle the post-install Composer event.
+     *
+     * @param  \Composer\Script\Event  $event
+     * @return void
+     */
+    public static function postInstall(\Composer\Script\Event $event)
+    {
+        require_once $event->getComposer()->getConfig()->get('vendor-dir').'/autoload.php';
+
+        static::clearCompiled();
+    }
+
+    /**
+     * Handle the post-update Composer event.
+     *
+     * @param  \Composer\Script\Event  $event
+     * @return void
+     */
+    public static function postUpdate(\Composer\Script\Event $event)
+    {
+        require_once $event->getComposer()->getConfig()->get('vendor-dir').'/autoload.php';
+
+        static::clearCompiled();
+    }
+
+    /**
+     * Handle the post-autoload-dump Composer event.
+     *
+     * @param  \Composer\Script\Event  $event
+     * @return void
+     */
+    public static function postAutoloadDump(\Composer\Script\Event $event)
+    {
+        //        require_once $event->getComposer()->getConfig()->get('vendor-dir').'/autoload.php';
+        //
+        //        static::clearCompiled();
+    }
+
+    /**
+     * Clear the cached Laravel bootstrapping files.
+     *
+     * @return void
+     */
+    protected static function clearCompiled()
+    {
+        $laravel = new \Illuminate\Foundation\Application(getcwd());
+
+        if (is_file($configPath = $laravel->getCachedConfigPath())) {
+            @unlink($configPath);
+        }
+
+        if (is_file($servicesPath = $laravel->getCachedServicesPath())) {
+            @unlink($servicesPath);
+        }
+
+        if (is_file($packagesPath = $laravel->getCachedPackagesPath())) {
+            @unlink($packagesPath);
+        }
+    }
+}
