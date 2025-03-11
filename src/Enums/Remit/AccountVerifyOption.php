d9ade2ddf9e755bf06942bf086e713a4a6d187a4
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-03-11 15:41:28 +0600

    account verification stubs added

---
+++ src/Enums/Remit/AccountVerifyOption.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Fintech\Core\Enums\Reload;
+
+use Fintech\Core\Attributes\Enumeration;
+use Fintech\Core\Enums\Color;
+use Fintech\Core\Traits\EnumHasSerialization;
+
+enum AccountVerifyOption: string implements \JsonSerializable
+{
+    use EnumHasSerialization;
+
+    #[Enumeration(color: Color::Yellow500)]
+    case BankTransfer = 'bank_transfer';
+
+    #[Enumeration(color: Color::Purple500)]
+    case Wallet = 'wallet';
+
+    #[Enumeration(color: Color::Blue500)]
+    case CashPickup = 'cash_pickup';
+}
